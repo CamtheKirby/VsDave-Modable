97ce27fb706363d14ba7ce0061286235b02f211e
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2024-06-08 21:28:09 -0500

    Fix Bat

--- Bulid.bat
+++ Bulid.bat
@@ -3,8 +3,13 @@ title Setup - PECG
 cd ..
 
 echo Installing dependencies, please wait...
+haxelib install lime 8.0.0
+haxelib install openfl
+haxelib install flixel 4.11.0
+haxelib run lime setup flixel
+haxelib run lime setup
 haxelib install actuate 1.9.0 
-haxelib git discord_rpc https://github.com/discord/discord-rpc
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib install flixel 4.11.0
 haxelib install flixel-addons 2.11.0
 haxelib install flixel-ui 2.6.1
