ce794f7f544c47c52b5f4f43e58d95ebfeb6d775
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2024-10-24 17:19:20 -0500

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -8,13 +8,17 @@ haxelib install openfl
 haxelib install flixel 4.11.0
 haxelib run lime setup flixel
 haxelib run lime setup
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install flixel-addons 2.9.0
 haxelib install actuate 1.9.0 
+haxelib install hxCodec 2.5.1          
+haxelib install linc_luajit
+haxelib install hscript
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib install flixel 4.11.0
-haxelib install flixel-addons 2.11.0
-haxelib install flixel-ui 2.6.1
-haxelib install hscript 2.5.0
-haxelib install hxCodec 2.5.1
-haxelib install hxcpp 4.3.2
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib install hxcpp-debug-server
+haxelib list
